ecaacefc5448419bfa6069c09ba7632f232f0b27
mmann30 <michael.l.mann13@gmail.com>
2017-06-04 14:48:47 -0400

    fully functional script

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ var queryTerm = "";
 var limit = 10;		// number of gifs to display
 var rating = "g";  	// gif rating
 
-var arrIngredients = ["chicken", "hamburger", "rice", "tomato", "potato", "corn"];
+var arrIngredients = ["chicken", "hamburger", "tacos", "salad", "french fries", "bacon", "pasta", "dessert"];
 
 var queryURLBase = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey;
 
@@ -21,13 +21,13 @@ function renderButtons() {
 	$("#button-display").empty();
 
 	for(var i = 0; i < arrIngredients.length; i++){
-		var $button = $("<button>");
-		$button.addClass("giphy btn btn-primary");
-		$button.attr("data-value", arrIngredients[i]);
-		$button.text(arrIngredients[i]);
+		var button = $("<button>");
+		button.addClass("giphy btn btn-primary");
+		button.attr("data-value", arrIngredients[i]);
+		button.text(arrIngredients[i]);
 
-		$("#button-display").append($button);
-		console.log(arrIngredients[i]);
+		$("#button-display").append(button);
+		console.log(arrIngredients[i]); 
 	}
 }
 
@@ -43,15 +43,26 @@ $("#submit-ingredient").on("click", function(event) {
 	renderButtons();
 });
 
-
-// TODO: Create gif pause/play event
-// TODO: 
+$(document).on("click", ".gif", function(event){
+	//  Sets state of the gif
+	var state = $(this).attr("data-state");
+	//  Checks the state and either animates or stops gif on.click
+	if(state === "still"){
+		$(this).attr("src", $(this).attr("data-animate"));
+		$(this).attr("data-state", "animate");
+	} else {
+		$(this).attr("src", $(this).attr("data-still"));
+		$(this).attr("data-state", "still");
+	}
+});
 
 // .on("click") triggers the AJAX call
 // add click event to document so it applies to newly generated buttons
 $(document).on("click", ".giphy", function(event){
+	// clears previously displayed gifs
+	$("#giphy-display").empty();
 	
-	var queryTerm = $(this).data("value") + "+recipie";
+	var queryTerm = $(this).data("value") + "+recipe";
 	console.log(queryTerm);
 	var queryURLNew = queryURLBase + "&q=" + queryTerm;
 
@@ -64,9 +75,22 @@ $(document).on("click", ".giphy", function(event){
 		console.log(queryURLNew);
 		console.log(response);
 		
-		// TODO:  create div space for each gif (for loop) 
-		
+		// Creates and displays number of gifs based on limit value
+		for(var i = 0; i < limit; i++){ 
 
+			var giphyURLStill = response.data[i].images.fixed_width_still.url;
+			var giphyURLAnimate = response.data[i].images.fixed_width.url;
+			var giphyImage = $("<img>");
+
+			giphyImage.attr("src", giphyURLStill);
+			giphyImage.attr("data-state", "still");
+			giphyImage.attr("data-still", giphyURLStill);
+			giphyImage.attr("data-animate", giphyURLAnimate);
+			giphyImage.attr("alt", "gif image");
+			giphyImage.addClass("gif");
+			$("#giphy-display").prepend(giphyImage);
+
+		}
 		// class=gif for each data-state=still/animate ...?
 		// TODO:  limit display to 10 gifs
 		// TODO:  limit gifs shown to "g" rating
